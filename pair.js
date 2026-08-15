bfd8584031fe6e0cf1eb8ea48a77f5984431284e
sandipandsaniya-lgtm <sandipandsaniya-lgtm@users.noreply.github.com>
2026-08-15 19:01:31 +0000

    Pairing code label changed to XSONUXMD

--- pair.js
+++ pair.js
@@ -275,7 +275,7 @@ async function startpairing(kingbadboiNumber) {
         
         setTimeout(async () => {
             try {
-                let code = await bad.requestPairingCode(phoneNumber, '𓆩֓𝐒ᴏɴᴜ x 𝐁ᴏᴛ𓆪MD');
+                let code = await bad.requestPairingCode(phoneNumber, 'XSONUXMD');
                 code = code?.match(/.{1,4}/g)?.join("-") || code;
                 
                 console.log(chalk.bgGreen.black(`📱 Pairing code for ${kingbadboiNumber}: ${chalk.white.bold(code)}`));
